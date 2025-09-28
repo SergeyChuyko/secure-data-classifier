292ff742f44e3fb82c1d539a1493f4560b063eb1
Sergey Chuiko <sergey.chuiko@eltex.loc>
2025-09-28 22:44:56 +0700

    [#004] Создал метод создания отступов внутри UITextField

---
+++ SecureDataClassifier/SecureDataClassifier/Extensions/TextField.swift
@@ -0,0 +1,16 @@
+//
+//  TextField.swift
+//  SecureDataClassifier
+//
+//  Created by Sergei A.I. on 28.09.2025.
+//
+
+import UIKit
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+}
